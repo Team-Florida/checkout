7b40acbe804ea133be0276ca007446b7569df625
David Paul <davidpaul@Davids-MacBook-Pro.local>
2020-02-03 22:53:45 -0800

    small style changes to better format on proxy server

--- client/src/styles/Calendar/CalendarTitleWrapper.js
+++ client/src/styles/Calendar/CalendarTitleWrapper.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const CalendarTitleWrapper = styled.div`
     text-align: center;
-    height: 25px;
+    height: 47px;
     padding-top: 22px;
     /* padding-bottom: 37px; */
     `;
